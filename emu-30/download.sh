c022377e42e5455f07640ffb2464090d3bec6390
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-02-03 21:35:35 +0800

    Toolchain Gen script only works with bash

--- emu-30/download.sh
+++ emu-30/download.sh
@@ -4,3 +4,4 @@ VERSION='30'
 # https://developer.android.com/studio/releases/emulator#emulator_for_arm64_hosts
 ~/bin/repo init --quiet -u https://github.com/e666666/Emulator_30 -b emu-$VERSION-release --depth=1
 ~/bin/repo sync --quiet --current-branch -j4
+sed -i 's/COMMAND ${GEN_SDK} "--host=${HOST}"/COMMAND "bash" ${GEN_SDK} "--host=${HOST}"/' external/qemu/android/build/cmake/toolchain.cmake

92c2bd5635b24e6588252d5f52bba49ebe6e7f4c
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-02-03 18:43:22 +0800

    Reduce download

--- emu-30/prebuilts/download.sh
+++ emu-30/prebuilts/download.sh
@@ -6,8 +6,10 @@ pushd tarballs
 
 wget -c https://github.com/Kitware/CMake/releases/download/v3.10.2/cmake-3.10.2.tar.gz
 
-git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8
+git clone --depth 1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.17-4.8
 pushd x86_64-linux-glibc2.17-4.8
+git fetch origin e75585c30f139dfa2eee4707a60d8d6b9e7f406a
 git checkout e75585c30f139dfa2eee4707a60d8d6b9e7f406a
+popd
 
 popd
